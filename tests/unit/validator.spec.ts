0087135fc67d751ce2c953d0dfa8aafdaf9eb31e
Nutchy <nutchy@loomx.io>
2020-03-06 18:55:49 +0700

    add more testcase

--- tests/unit/validator.spec.ts
+++ tests/unit/validator.spec.ts
@@ -1,7 +1,6 @@
-import { isQuartoByHeadShape } from './../../src/utils/validator'
+import { isQuartoByHeadShape, isQuartoByBodyShape } from './../../src/utils/validator'
 import { createPiece } from './../../src/utils'
-import { basePieces } from './../../src/utils/mock'
-import { Color, BodyShape, HeadShape, Height, ISelecetedPiece, Piece } from '@/types'
+import { Color, BodyShape, HeadShape, Height, ISelecetedPiece } from '@/types'
 import { isQuartoByColor, isSubsetOfArray } from '../../src/utils/validator'
 
 describe('Should return true for ', () => {
@@ -40,6 +39,24 @@ describe('Should return true for ', () => {
       '03': createPiece(Color.Light, HeadShape.Solid, BodyShape.Round, Height.Short),
     }
     expect(isQuartoByHeadShape(pieces, HeadShape.Solid)).toBeTruthy();
+  })
+  it('isQuartoByBodyShape - Body(Round)', () => {
+    const pieces: ISelecetedPiece = {
+      '00': createPiece(Color.Light, HeadShape.Hollow, BodyShape.Round, Height.Short),
+      '01': createPiece(Color.Dark, HeadShape.Solid, BodyShape.Round, Height.Short),
+      '02': createPiece(Color.Light, HeadShape.Solid, BodyShape.Round, Height.Tall),
+      '03': createPiece(Color.Dark, HeadShape.Hollow, BodyShape.Round, Height.Short),
+    }
+    expect(isQuartoByBodyShape(pieces, BodyShape.Round)).toBeTruthy();
+  })
+  it('isQuartoByBodyShape - Body(Square)', () => {
+    const pieces: ISelecetedPiece = {
+      '30': createPiece(Color.Dark, HeadShape.Solid, BodyShape.Square, Height.Short),
+      '21': createPiece(Color.Light, HeadShape.Hollow, BodyShape.Square, Height.Short),
+      '12': createPiece(Color.Dark, HeadShape.Solid, BodyShape.Square, Height.Tall),
+      '03': createPiece(Color.Light, HeadShape.Hollow, BodyShape.Square, Height.Short),
+    }
+    expect(isQuartoByBodyShape(pieces, BodyShape.Square)).toBeTruthy();
   })
 
   it('should return false', () => {
